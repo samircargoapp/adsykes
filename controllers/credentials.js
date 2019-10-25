4dbf135d49a1846758d1307c4ef92bf24b0eda0d
sruidiaz@cargoapp.com.co <sruidiaz@cargoapp.com.co>
2019-10-25 08:48:10 -0500

    conexión a firebase

--- controllers/credentials.js
+++ controllers/credentials.js
@@ -1,23 +1,54 @@
+// Use LDAP
+//const ldap = require('ldapjs');
+
+const admin = require("firebase-admin");
+const serviceAccount = require("../config/swsykes-6dbbd-firebase-adminsdk-tomxs-bcfaec8579.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://swsykes-6dbbd.firebaseio.com"
+});
+
 // packages
 const { validationResult } = require('express-validator')
 
 // Constants
 const constants = require('../constants/Messages')
-let entity = 'credenciales'
 
+
+let entity = 'credenciales'
 exports.signin = async (req, res) => {
-    try {
+   try {
       const errors = validationResult(req)
       if (errors.isEmpty()) {
-
-        // let result = await credentialsServices.signin(req)
-        let result = req.body
-        res.status(200).send({ code: 1, message: constants.messages.signin.success[1], response: result })
-
+         let result = await getUser("/restricted_access/users/username1");
+         res.status(200).send({ code: 1, message: constants.messages.signin.success[1], response: result })
       } else {
         res.status(400).send({ code: 4, message: constants.messages.signin.error[1], error: errors.array() })
       }
-    } catch (error) {
-        res.status(400).send({ code: 0, message: constants.messages.signin.error[1], error: error })
-    }
+   } catch (error) {
+      res.status(500).send({ code: 0, message: constants.messages.signin.error[1], error: error })
+   }
+}
+
+exports.listUsers = async (req, res) =>{
+   try {
+      let result = await getUser("/restricted_access/users");
+      res.status(200).send({ code: 1, message: constants.messages.signin.success[1], response: result })
+   } catch (error) {
+      res.status(500).send({ code: 0, message: constants.messages.signin.error[1], error: error })
+   }
+}
+
+const getUser = async (param) =>{
+   try {
+      let database = admin.database();
+      let ref = database.ref(param);
+      let value = ref.once("value", function(snapshot) {
+         return snapshot.val()
+      })
+      return value
+   } catch (error) {
+      throw new Error(error)
+   }
 }
--- routes/v1/credentials.js
+++ routes/v1/credentials.js
@@ -10,5 +10,6 @@ const credentialsControllers = require('../../controllers/credentials')
 // Definiendo rutas de credenciales
 credentialsRoutes
   .post('/api/v1/credentials/signin', validateCredentials, credentialsControllers.signin)
+  .get('/api/v1/users', credentialsControllers.listUsers)
 
 module.exports = credentialsRoutes
